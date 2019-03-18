3afdf12a618b2ef595a7ba783445ac79aecb4bcc
amdan <amdan@128.4.150.72>
2019-03-17 22:53:02 -0400

    current

--- SudokuBLL/src/test/java/pkgGame/SudokuTest.java
+++ SudokuBLL/src/test/java/pkgGame/SudokuTest.java
@@ -8,7 +8,7 @@ public class SudokuTest {
 	
 	public SudokuTest() {
 	}
-	//a
+	
 	@Test
 	public void TestRegionNbr() throws Exception {
 		int[][] puzzle= {{1,2,3,4},{2,3,4,1},{3,4,1,2},{4,1,2,3}};
