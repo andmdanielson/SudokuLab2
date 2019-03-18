2bb27b82d10fe0a265b036fdb8cd18b668f1962d
amdan <amdan@128.4.150.72>
2019-03-17 22:40:25 -0400

    get region

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -5,5 +5,62 @@ import pkgHelper.LatinSquare;
 import java.util.Arrays;
 
 public class Sudoku extends LatinSquare{
-
+	
+	private int iSize;
+	private int iSqrtSize;
+	
+	public Sudoku(int size) 
+		throws java.lang.Exception{
+		super();
+		iSize=size;
+		double root=Math.sqrt(size);
+		if (root-Math.floor(root)==0) {
+			iSqrtSize=(int) root;
+		}
+		else {
+			throw new Exception();
+		}
+	}
+	
+	public Sudoku(int[][] puzzle)
+		throws java.lang.Exception{
+		super(puzzle);
+		iSize=puzzle.length;
+		double root=Math.sqrt(puzzle.length);
+		if (root-Math.floor(root)==0) {
+			iSqrtSize=(int) root;
+		}
+		else {
+			throw new Exception();
+		}
+	}
+	
+	public int[][] getPuzzle(){
+		return getLatinSquare();
+	}
+	
+	public int[] getRegion(int iRegion) throws Exception {
+		if (iRegion+1>iSize) {
+			throw new Exception();
+		}
+		int[] region=new int[iSize];
+		int RowI=(iRegion/iSqrtSize)*iSqrtSize;
+		int RowF=RowI+iSqrtSize;
+		int ColI=(iRegion%iSqrtSize)*iSqrtSize;
+		int ColF=ColI+iSqrtSize;
+		int idx=0;
+		for(int iRow=RowI; iRow<RowF; iRow++) {
+			for(int iCol=ColI; iCol<ColF; iCol++) {
+				int[] row=getRow(iRow);
+				region[idx]=row[iCol];
+				idx++;
+			}
+		}
+		return region;
+	}
+	
+	public int[] getRegion(int iCol, int iRow) throws Exception {
+		int region=(iCol/iSqrtSize)+(iRow/iSqrtSize)*iSqrtSize;
+		return getRegion(region);
+	}
 }
---
+++ SudokuBLL/src/test/java/pkgGame/SudokuTest.java
@@ -0,0 +1,22 @@
+package pkgGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SudokuTest {
+	
+	public SudokuTest() {
+	}
+	
+	@Test
+	public void TestRegionNbr() throws Exception {
+		int[][] puzzle= {{1,2,3,4},{2,3,4,1},{3,4,1,2},{4,1,2,3}};
+		Sudoku myS=new Sudoku(puzzle);
+		int[] expected= {3,4,4,1};
+		int[] actual=myS.getRegion(2, 1);
+		assertArrayEquals(expected, actual);
+	}
+	
+
+}
